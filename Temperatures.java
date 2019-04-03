7175b35b7c6916bc1303e4c2a09db18505ac9a6f
Stasha73 <46947808+Stasha73@users.noreply.github.com>
2019-04-02 20:39:58 -0500

    Add files via upload

--- Temperatures.java
+++ Temperatures.java
@@ -17,7 +17,7 @@ public class Temperatures {
 
         String hours[] = {"7 AM", "3 PM", "7 PM", "3 AM"};  //string array (hours)
         String days[] = {"Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"}; //string array(days)
-        //int columnTotal = 0;
+        //
         for (int row = 0; row < temp.length; row++) { //starts the row for loops
             System.out.print(hours[ row ] + ": ");
             for (int column = 0; column < temp[ row ].length; column++) {// starts the column for loop
@@ -29,6 +29,7 @@ public class Temperatures {
         System.out.println();
 
         System.out.println("Based on that data, the following are the average temperatures for the week");
+        //calculate the average temperatures for the week
         //column becomes the outer loop, iterates first
         int sum = 0;
         for (int column = 0; column < days.length; column++) { //starts the column for loop (outer loop)
@@ -42,14 +43,17 @@ public class Temperatures {
             }
             System.out.print(sum / hours.length);
             sum = 0;
+            //sum = 0 resets the loop in order to get the average each day of the week
             System.out.println();
 
         }
         System.out.println("  ");
+        //calculate the average of the temperature per specific time of the day
         int sum2 = 0;
 
         for (int row = 0; row < hours.length; row++) { //starts the row loop (outer loop)
             System.out.print(hours[ row ] + ": ");
+            //sum 2 = 0 resets in order to calculate the average per specific time of the day (per row)
             sum2= 0;
             for (int column = 0; column < days.length; column++) {
                 sum2 += temp[ row ][ column ];
@@ -59,7 +63,7 @@ public class Temperatures {
 
         }
         System.out.println();
-
+        //calculate the final average temperature for the week
         System.out.println("The final average temperature for the week was: ");
         System.out.println();
         int sum3 = 0;
